22e8490bb2d68b71236123868d31225e4f858bd1
HanushRegupathy <117075065+HanushRegupathy@users.noreply.github.com>
2022-11-11 16:20:02 +0530

    Update ProgramA.java

--- ProgramA.java
+++ ProgramA.java
@@ -2,6 +2,6 @@ public class ProgramA {
 public static void main(String[] args) {
 System.out.println("Hello World");
 System.out.println("Hello India");
-System.out.println("Hello TN");
+System.out.println("Hello Tamil Nadu");
 }
 }
